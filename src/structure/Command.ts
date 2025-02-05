f775500d008ae789080b4404f79e281b8c83f782
Toddy <jaelsonkaciel4@gmail.com>
2025-02-04 22:06:36 -0300

    resolving error

--- src/structure/Command.ts
+++ src/structure/Command.ts
@@ -1,10 +1,16 @@
 import { CommandPayload, CommandConstructor } from '../types/global';
 
 export class Command {
+    public name: string;
+    public aliases: string[];
+    
     public constructor({
         name,
         aliases,
-    }: CommandConstructor) {};
+    }: CommandConstructor) {
+        this.name = name;
+        this.aliases = aliases ?? [];
+    };
 
     public execute(payload: CommandPayload) {};
 }
