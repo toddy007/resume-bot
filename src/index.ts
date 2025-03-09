347546e2bd9678f87fe901b2b3250d57cf625ec7
Toddy <jaelsonkaciel4@gmail.com>
2025-03-09 13:54:50 -0300

    Toc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Client } from './structure/Client';
 
-export const client = new Client<true>({ intents: [36864]}); // intents in https://discord-intents-calculator.vercel.app/
+export const client = new Client<true>({ intents: [33281]}); // intents in https://discord-intents-calculator.vercel.app/
 
 client.run();
