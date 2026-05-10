9550687a098af021323ffe7521ae151da44a67a4
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-05-10 02:23:05 +0000

    transforming into abstract

--- src/structure/Command.ts
+++ src/structure/Command.ts
@@ -1,16 +1,20 @@
 import { CommandPayload, CommandConstructor } from '../types/global';
+import { SlashCommandBuilder } from 'discord.js';
 
-export class Command {
+export abstract class Command {
     public name: string;
     public aliases?: string[];
+    public slashCommandData?: SlashCommandBuilder;
     
     public constructor({
         name,
         aliases,
+        slashCommandData,
     }: CommandConstructor) {
         this.name = name;
         this.aliases = aliases ?? [];
+        this.slashCommandData = slashCommandData;
     };
 
-    public execute(payload: CommandPayload) {};
+    public abstract execute(payload: CommandPayload): unknown;
 }
