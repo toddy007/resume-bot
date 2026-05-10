9fd9e211f88563fd8d7482c62552db058e977d26
Jaelson Kaciel <111665673+toddy007@users.noreply.github.com>
2026-05-10 02:30:52 +0000

    editing ping

--- src/client/commands/general/ping.ts
+++ src/client/commands/general/ping.ts
@@ -1,15 +1,19 @@
 import { Command } from '../../../structure/Command';
 import { CommandPayload } from '../../../types/global';
+import { SlashCommandBuilder } from 'discord.js';
 
 export default class PingCommand extends Command {
     public constructor() {
         super({
             name: 'ping',
             aliases: ['pong'],
+            slashCommandData: new SlashCommandBuilder()
+                .setName('ping')
+                .setDescription('Mostra a latência do bot'),
         });
     }
 
-    public async execute({ client, message }: CommandPayload) {
-        message.reply('Pong! `' + client.ws.ping + 'ms`');
+    public async execute({ client, context }: CommandPayload) {
+        this.reply(context, 'Pong! `' + client.ws.ping + 'ms`');
     }
 }
