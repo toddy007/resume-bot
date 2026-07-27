d272c7d09a2d7678d75318387fb6e22d4b5729d2
toddy007 <jaelsonkaciel4@gmail.com>
2026-07-26 21:07:29 -0300

    feat: updating resume
    -making it search up to 200 messages
    -making params to search after or before a message id
    -making it ignore it owns messages

--- src/client/commands/general/resume.ts
+++ src/client/commands/general/resume.ts
@@ -8,14 +8,34 @@ export default class ResumeCommand extends Command {
         super({
             name: 'resume',
             aliases: ['resumir', 'r'],
-            slashCommandData: new SlashCommandBuilder()
+            slashCommandData: (new SlashCommandBuilder()
                 .setName('resume')
-                .setDescription('Resume a conversa do chat'),
+                .setDescription('Resume a conversa do chat')
+                .addStringOption((option) =>
+                    option
+                        .setName('message_id')
+                        .setDescription('O bot vai coletar mensagens após/antes dessa mensagem')
+                        .setMinLength(17)
+                        .setMaxLength(21)
+                )
+                .addStringOption((option) =>
+                    option
+                        .setName('order')
+                        .setDescription('A ordem em que vai coletar')
+                        .setChoices([
+                            { name: 'Before', value: 'before' },
+                            { name: 'After', value: 'after' },
+                        ])
+                )) as unknown as SlashCommandBuilder
         });
     }
 
-    public async execute({ client, context }: CommandPayload) {
+    public async execute({ client, context, args }: CommandPayload) {
         if (context.author.id !== '1315730837152731239') return context.reply({ content: 'nao', flags: ["Ephemeral"] });
+
+        const { reference } = context;
+        const messageId = reference?.messageId || args[0] || context.getString({ name: 'message_id' });
+        const order = args[1] || context.getString({ name: 'order' });
 
         const isInteraction = context.isInteractionContext(context.context);
         if (isInteraction) await context.deferReply();
@@ -23,8 +43,24 @@ export default class ResumeCommand extends Command {
         const { channel } = context;
         if (!channel || !channel.isTextBased()) return;
 
-        const messages = await channel.messages.fetch({ limit: 100 });
-        const ordered = [...messages.values()].reverse().filter(msg => msg.content);
+        if (messageId) {
+            const message = await channel.messages.fetch(messageId).catch(() => null);
+
+            if (!message)
+                return context.reply({ content: 'Não encontrei a mensagem com o ID fornecido.', flags: ["Ephemeral"] });
+        }
+
+        let option = (order && order === 'before' ? { before: messageId } : { after: messageId }) as { before?: string } | { after?: string };
+        if (order && !messageId)
+            option = { after: undefined };
+
+        const messages = await channel.messages.fetch({ limit: 100, ...option });
+        let anotherMessages;
+        if (messages.size === 100)
+            anotherMessages = await channel.messages.fetch({ before: messages.last()?.id, limit: 100 });
+        const ordered = [...messages.values()].reverse().filter(msg => msg.content && msg.author.id !== client.user.id);
+        if (anotherMessages && anotherMessages.size > 0)
+            ordered.unshift(...[...anotherMessages.values()].reverse().filter(msg => msg.content && msg.author.id !== client.user.id));
 
         const text = ordered
             .map((msg) => `${msg.author.username} (${msg.author.id})\n${msg.content}`)
@@ -36,7 +72,7 @@ export default class ResumeCommand extends Command {
                 {
                     role: "system",
                     content:
-                        `Você é um bot de Discord, sua função é resumir mensagens em um canal de texto e retornar os assuntos em tópicos com os principais assuntos, você não retorna nada além dos topicos, você será mais enfatico no retorno, eu quero que você diga o que cada participante estava fazendo durante a conversa, não em uma parte separada do retorno, sem mostrar o id deles, somente nome. Não quero considerações finais suas.
+                        `Você é um bot de Discord, sua função é resumir mensagens em um canal de texto e retornar os assuntos em tópicos com os principais assuntos, as mensagens vão estar das mais antigas para as mais novad, você vai dar titulos para os tópicos, você não retorna nada além dos topicos, você será mais enfatico no retorno, eu quero que você diga o que cada participante estava fazendo durante a conversa, não em uma parte separada do retorno, sem mostrar o id deles, somente nome. Não quero considerações finais suas.
                         Você vai receber mensagens no seguinte formato:
                         *nome do autor da mensagem* ( *id do autor da mensagem* )
                         *conteúdo da mensagem*
